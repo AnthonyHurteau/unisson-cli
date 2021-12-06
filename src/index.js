3968cd3e49094a184edc626f9f39dfe2b76e95c9
Unknown <anthony.hurteau@radio-canada.ca>
2021-12-06 09:19:35 -0500

    save

--- src/index.js
+++ src/index.js
@@ -7,54 +7,116 @@ const chalk = require("chalk");
 const template = require("./template");
 const shelljs = require("shelljs");
 
+const operations = { create: "create", c: "c", help: "help", h: "h" };
+const operationsList = ["create", "help"];
+const types = { app: "app", a: "a", component: "component", c: "c" };
+const typesList = ["app", "component"];
+const args = { operation: process.argv[2], type: process.argv[3], name: process.argv[4] };
+
 const templates = fs.readdirSync(path.join(__dirname, "templates"));
 
 const questions = [
+  // Can use this when we will have TS template
+  // {
+  //   name: "template",
+  //   type: "list",
+  //   message: "Which Unisson template do you wish to generate?",
+  //   choices: templates,
+  // },
   {
-    name: "template",
+    name: "operation",
     type: "list",
-    message: "Which Unisson template do you wish to generate?",
-    choices: templates,
+    message: "Which operation do you wish to perform?",
+    when: () => !args.operation,
+    choices: operationsList,
+  },
+  {
+    name: "type",
+    type: "list",
+    message: "Which type of element do you wish to create?",
+    when: (answers) =>
+      !args.type &&
+      (answers.operation === "create" || args.operation === operations.create || args.operation === operations.c),
+    choices: typesList,
   },
   {
     name: "name",
     type: "input",
     message: "Project Name:",
+    when: (answers) =>
+      !args.name &&
+      (answers.operation === "create" || args.operation === operations.create || args.operation === operations.c),
     validate: (input) => {
-      if (/^([A-Za-z\-\_\d])+$/.test(input)) return true;
-      else return "Project name may only include letters, numbers, underscores and dashes.";
+      if (/^([A-Za-z\-\_\d])+$/.test(input)) {
+        return true;
+      } else {
+        return "Project name may only include letters, numbers, underscores and dashes.";
+      }
     },
   },
 ];
 
 const currentDirectory = process.cwd();
+const operationArg = process.argv[2] ? process.argv[2] : null;
 
-console.log(process.argv[2]);
+if ([operations.create, operations.c, null].includes(operationArg)) {
+  program();
+} else if ([operations.help, operations.h].includes(operationArg)) {
+  showHelpMessage();
+} else {
+  console.log("");
+  console.log(chalk.red(`Argument <operation> ${operationArg} is not valid.`));
+  showHelpMessage();
+}
 
-inquirer.prompt(questions).then((answers) => {
-  const projectTemplate = answers["template"];
-  const projectName = answers["name"];
-  const templatePath = path.join(__dirname, "templates", projectTemplate);
-  const targetPath = path.join(currentDirectory, projectName);
+function program() {
+  inquirer.prompt(questions).then((answers) => {
+    // Can use this when we will have TS template
+    // const projectTemplate = answers["template"];
+    const projectTemplate = templates[0];
+    const operation = answers["operation"];
+    const projectName = answers["name"] ?? "";
+    const templatePath = path.join(__dirname, "templates", projectTemplate);
 
-  // if (!checkProjectName(projectName)) {
-  //   return;
-  // }
+    const targetPath = path.join(currentDirectory, projectName);
 
-  // if (!createProject(targetPath)) {
-  //   return;
-  // }
+    if (operation === operations.help || operations.h) {
+      showHelpMessage();
+      return false;
+    }
 
-  // createDirectoryContents(templatePath, projectName, projectName);
+    // if (!checkProjectName(projectName)) {
+    //   return;
+    // }
 
-  // if (!postProcess(templatePath, targetPath)) {
-  //   return;
-  // }
+    // if (!createProject(targetPath)) {
+    //   return;
+    // }
 
-  showMessage(projectName);
-});
+    // createDirectoryContents(templatePath, projectName, projectName);
 
-function showMessage(projectName) {
+    // if (!postProcess(templatePath, targetPath)) {
+    //   return;
+    // }
+
+    showCompleteMessage(projectName);
+  });
+}
+
+function showHelpMessage() {
+  console.log("");
+  console.log("Please use the following syntax: " + chalk.green("unission <operation> <type> <name>"));
+  console.log("");
+  console.log(chalk.green("---- $operation ----"));
+  console.log(chalk.blueBright(operations.help) + " or " + chalk.blueBright(operations.h));
+  console.log(chalk.blueBright(operations.create) + " or " + chalk.blueBright(operations.c));
+  console.log("");
+  console.log(chalk.green("---- create $type ----"));
+  console.log(chalk.blueBright(types.app) + " or " + chalk.blueBright(types.a));
+  console.log(chalk.blueBright(types.component) + " or " + chalk.blueBright(types.c));
+}
+
+function showCompleteMessage(projectName) {
   console.log("");
   console.log(chalk.green("Unisson template created successfully!"));
   console.log(chalk.green(`Access the project directory using cd ${projectName}`));
